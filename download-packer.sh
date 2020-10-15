b2e68bc9ae739654b1fddeba187690694faf8c63
pikachunakapika <13935418+pikachunakapika@users.noreply.github.com>
2020-10-15 16:00:25 +0200

    Update download-packer.sh

--- download-packer.sh
+++ download-packer.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
-curl -O https://raw.githubusercontent.com/picodotdev/alis/master/alis.conf
-curl -O https://raw.githubusercontent.com/picodotdev/alis/master/alis.sh
-curl -O https://raw.githubusercontent.com/picodotdev/alis/master/alis-packer.sh
+curl -O https://raw.githubusercontent.com/pikachunakapika/alis/master/alis.conf
+curl -O https://raw.githubusercontent.com/pikachunakapika/alis/master/alis.sh
+curl -O https://raw.githubusercontent.com/pikachunakapika/alis/master/alis-packer.sh
 mkdir -p packer/
-curl -o packer/alis-packer-efi-btrfs-luks-lvm-systemd.json https://raw.githubusercontent.com/picodotdev/alis/master/packer/alis-packer-efi-btrfs-luks-lvm-systemd.json
-curl -o packer/alis-packer-efi-ext4-grub-gnome.json https://raw.githubusercontent.com/picodotdev/alis/master/packer/alis-packer-efi-ext4-grub-gnome.json
-curl -o packer/alis-packer-efi-ext4-grub-kde.json https://raw.githubusercontent.com/picodotdev/alis/master/packer/alis-packer-efi-ext4-grub-kde.json
-curl -o packer/alis-packer-efi-ext4-grub-xfce.json https://raw.githubusercontent.com/picodotdev/alis/master/packer/alis-packer-efi-ext4-grub-xfce.json
-curl -o packer/alis-packer-efi-ext4-luks-lvm-grub.json https://raw.githubusercontent.com/picodotdev/alis/master/packer/alis-packer-efi-ext4-luks-lvm-grub.json
-curl -o packer/alis-packer-efi-f2fs-luks-lvm-systemd.json https://raw.githubusercontent.com/picodotdev/alis/master/packer/alis-packer-efi-f2fs-luks-lvm-systemd.json
+curl -o packer/alis-packer-efi-btrfs-luks-lvm-systemd.json https://raw.githubusercontent.com/pikachunakapika/alis/master/packer/alis-packer-efi-btrfs-luks-lvm-systemd.json
+curl -o packer/alis-packer-efi-ext4-grub-gnome.json https://raw.githubusercontent.com/pikachunakapika/alis/master/packer/alis-packer-efi-ext4-grub-gnome.json
+curl -o packer/alis-packer-efi-ext4-grub-kde.json https://raw.githubusercontent.com/pikachunakapika/alis/master/packer/alis-packer-efi-ext4-grub-kde.json
+curl -o packer/alis-packer-efi-ext4-grub-xfce.json https://raw.githubusercontent.com/pikachunakapika/alis/master/packer/alis-packer-efi-ext4-grub-xfce.json
+curl -o packer/alis-packer-efi-ext4-luks-lvm-grub.json https://raw.githubusercontent.com/pikachunakapika/alis/master/packer/alis-packer-efi-ext4-luks-lvm-grub.json
+curl -o packer/alis-packer-efi-f2fs-luks-lvm-systemd.json https://raw.githubusercontent.com/pikachunakapika/alis/master/packer/alis-packer-efi-f2fs-luks-lvm-systemd.json
 
 chmod +x ./alis-packer.sh
 ./alis-packer.sh
